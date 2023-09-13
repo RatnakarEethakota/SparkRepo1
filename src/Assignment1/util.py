3ae7274ec540db5bf33896e9a601f09af6217f42
Ratnakar Eethakota <ratnakar.eethakota@diggibyte.com>
2023-09-13 11:32:55 +0530

    Code Updated

--- src/Assignment1/util.py
+++ src/Assignment1/util.py
@@ -9,14 +9,14 @@ def merge(user_df,transaction_df):
 def UniqueLocation(total_df):
     # a): Count of unique locations where each product is sold
     product_locations_df = total_df.groupBy("product_id").agg(countDistinct("location ").alias("UniqueLocations"))
-    product_locations_df.show()
+    return product_locations_df
 def ProductBought(total_df):
     # b): Find out products bought by each user
     user_products_df = total_df.groupBy("userid").agg(collect_list("product_id").alias("ProductsBought"))
-    user_products_df.show()
+    return user_products_df
 def TotalSpending(total_df):
     # c): Total spending done by each user on each product
     user_product_spending_df = total_df.groupBy("userid", "product_id").agg(sum("price").alias("TotalSpending"))
-    user_product_spending_df.show()
+    return user_product_spending_df
 
 
